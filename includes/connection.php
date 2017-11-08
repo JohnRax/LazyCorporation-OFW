d77b3048c0b5624385b00eed30d835280fe759b3
lazzytechnologies <33306216+lazzytechnologies@users.noreply.github.com>
2017-11-08 16:30:47 +0800

    connection

--- includes/connection.php
+++ includes/connection.php
@@ -13,7 +13,7 @@
 
 	
 	try {
-		$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,0,":/cloudsql/lazzyworks-185201:asia-northeast1:lazzyworksdb");
+		$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,0,"/cloudsql/lazzyworks-185201:asia-northeast1:lazzyworksdb");
 		if($connection)
 		{
 			
