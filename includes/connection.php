638047102159f082cf9f8685373b36d6253fe97d
lazzytechnologies <33306216+lazzytechnologies@users.noreply.github.com>
2017-11-08 16:15:10 +0800

    connection exception

--- includes/connection.php
+++ includes/connection.php
@@ -4,7 +4,6 @@
 	$db['db_user']='root';
 	$db['db_pass']='root';
 	$db['db_name']='lazycorporation-ofwdatabase';
-	$db['db_socket']='';
 
 	foreach ($db as $key => $value) {
 		
@@ -12,8 +11,9 @@
 	}
 
 
-	$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,0,":/cloudsql/lazzyworks-185201:asia-northeast1:lazzyworksdb");
+	
 	try {
+		$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,0,":/cloudsql/lazzyworks-185201:asia-northeast1:lazzyworksdb");
 		if($connection)
 		{
 			
