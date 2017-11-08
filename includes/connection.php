f156682cfc196d24d9df7bb70163b290dc3ee0d4
lazzytechnologies <33306216+lazzytechnologies@users.noreply.github.com>
2017-11-08 13:48:50 +0800

    connection

--- includes/connection.php
+++ includes/connection.php
@@ -10,11 +10,9 @@
 		define(strtoupper($key),$value);
 	}
 
-<<<<<<< HEAD
+
 	$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,null,'/cloudsql/lazzyworks-185201:asia-northeast1:lazzyworksdb');
-=======
-	$connection = mysqli_connect(null,DB_USER,DB_PASS,DB_NAME,null);
->>>>>>> d08f839a368c3fc6d3873e7c64cf822fe8217ccd
+
 	if($connection)
 	{
 		
